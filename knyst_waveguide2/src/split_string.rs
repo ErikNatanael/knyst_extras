96d24adf2e26bf6f35038a8cdc15312be283ca94
Erik Natanael Gustafsson <erik@eriknatanael.com>
2024-03-09 11:10:26 +0100

    Better high notes

--- knyst_waveguide2/src/split_string.rs
+++ knyst_waveguide2/src/split_string.rs
@@ -85,7 +85,7 @@ impl SplitWaveguide {
         let freq = freq * 2.0 * 0.9929;
         let (mut delay0_time, mut delay1_time) = delay_times(freq, position);
         // Why is it 1.5 and not 1.0? Idk, but it keeps the top pitches in tune without the lp filter
-        static FEEDBACK_DELAY_COMPENSATION: f64 = 1.5;
+        static FEEDBACK_DELAY_COMPENSATION: f64 = 0.5;
         // Make sure there cannot be a negative time delay
         delay0_time = delay0_time * sample_rate - FEEDBACK_DELAY_COMPENSATION
             + delay_compensation
